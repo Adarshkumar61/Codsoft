1588f1627d25bffe30bc246bee1a87b97f979f5d
Adarshkumar61 <adarshb2k61@gmail.com>
2025-03-06 21:57:40 +0530

    created a rps game

---
+++ rock_paper_scissor.py
@@ -0,0 +1,21 @@
+import random
+
+def game():
+    user_choice = input("Enter your choice: ")
+    choices = ["rock", "paper", "scissor"]
+    com_choice = random.choice(choices)
+    
+    if user_choice == com_choice:
+        print("computer choosed: ", com_choice)
+        print("Match tie")
+    elif(user_choice == "rock" and com_choice == "scissor") or \
+        (user_choice == "paper" and com_choice == "rock") or \
+        (user_choice == "scissor" and com_choice == "paper"):
+        print("computer choosed: ", com_choice)
+        print("You win... Hurrrahh.....")
+        
+    else:
+        print("computer choosed: ", com_choice)
+        print("Computer win")
+game()
+    
