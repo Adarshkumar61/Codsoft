142750ae4dca53f56733b8a23605b469fc84d119
Adarshkumar61 <adarshb2k61@gmail.com>
2025-03-18 21:01:10 +0530

    upgraded

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,19 @@
 import random
 import string
+
 def random_pass(length):
     if length < 4:
         return "password length should atleast 4 digits.."
     char = string.ascii_letters + string.digits
     password = " ".join(random.choice(char) for i in range(length))
-    return password    
+    return password
+
 while True:
     try:
         length = int(input("Enter your password length: "))
         break
     except ValueError:
         print("invalid input..")
+        
 password = random_pass(length)
 print("Generated password: ", password)
