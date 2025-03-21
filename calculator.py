5ae04a73da78e03541ad95b8d299394d5be2215b
Adarshkumar61 <adarshb2k61@gmail.com>
2025-03-21 18:35:41 +0530

    codsoft_taskno_2

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
 def calculator():
     input1 = float(input("Enter your first number: "))
+    
     operation =  input("Enter what do you want to do (+, -, *, /): ")
+    
     input2 = float(input("Enter your second number: "))
+    
     if operation == "+":
         result = input1 + input2
     elif operation == "-":
@@ -15,5 +18,11 @@ def calculator():
            result = input1 / input2
     else:
         print("invalid operator..")
-        print("result: ", result)
-calculator()                   
+        
+    print("result: ", result)
+    choice = input("Want to use again ?")
+    if choice == "yes".lower():
+        calculator()
+    else:
+        print("thanku for using calculator..")    
+calculator()                 
